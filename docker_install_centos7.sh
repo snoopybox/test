ede2d9981732a396a8e5e046b6b934f14ed2400d
snoopybox <hyeongjun.heo@gmail.com>
2017-09-09 14:49:01 +0900

    Create docker_install_centos7.sh

---
+++ docker_install_centos7.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl https://download.docker.com/linux/centos/docker-ce.repo > /etc/yum.repos.d/docker-ce.repo
+yum install -y yum-utils device-mapper-persistent-data lvm2 docker-ce yum-plugin-ovl
+systemctl enable docker
+systemctl start docker
