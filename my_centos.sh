2023b978e71e2d7bcafddd7a64ffd587ecf0b261
snoopybox <hyeongjun.heo@gmail.com>
2017-09-25 21:59:44 +0900

    Update my_centos.sh

--- my_centos.sh
+++ my_centos.sh
@@ -3,6 +3,8 @@
 sed -ci 's/^SELINUX=.\+/SELINUX=disabled/' /etc/selinux/config
 setenforce 0
 
+sed -i '/tsflags=nodocs/d' /etc/yum.conf
+
 yum -y install wget make gcc gcc-c++ vim man ntp xz gzip bzip2 net-tools bind-utils \
 traceroute sysstat lsof telnet tcpdump file
 
