74731dd87898184397f3316b1cb89cc2d35fc6e4
snoopybox <hyeongjun.heo@gmail.com>
2018-07-06 10:22:06 +0900

    Delete docker_install_centos7.sh

--- docker_install_centos7.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl https://download.docker.com/linux/centos/docker-ce.repo > /etc/yum.repos.d/docker-ce.repo
-yum install -y --enablerepo=extras yum-utils device-mapper-persistent-data lvm2 docker-ce yum-plugin-ovl
-systemctl enable docker
-systemctl start docker
