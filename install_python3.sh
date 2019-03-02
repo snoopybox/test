b490d9ccb7c5ec2d6191bcee3663759871a8d558
snoopybox <hyeongjun.heo@gmail.com>
2019-03-03 00:43:59 +0900

    Create install_python3.sh

---
+++ install_python3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PYTHON_VER=3.7.2
+PREFIX=/app/python3
+
+mkdir -p $PREFIX
+yum -y install wget make gcc xz openssl-devel bzip2-devel ncurses-devel \
+gdbm-devel xz-devel sqlite-devel readline-devel tk-devel libffi-devel
+
+wget https://www.python.org/ftp/python/${PYTHON_VER}/Python-${PYTHON_VER}.tar.xz
+tar xvf Python-${PYTHON_VER}.tar.xz
+cd Python-${PYTHON_VER}
+./configure --prefix=${PREFIX} --enable-shared --with-ensurepip=install
+CPU_CORE=$(lscpu | awk '/^CPU\(s\):/ {print $NF}')
+make -j${CPU_CORE}
+make install
+cd -
+echo "${PREFIX}/lib" > /etc/ld.so.conf.d/python3.conf
+ldconfig
+echo "export PATH=${PREFIX}/bin:\$PATH" >> /etc/profile
+source /etc/profile
+
+python3 -V
